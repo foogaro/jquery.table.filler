6aa1f9197580995fe26ab1d4e589b759e3dea7a8
Luigi Fugaro <l.fugaro@gmail.com>
2012-09-10 11:34:37 +0200

    First commit

---
+++ jquery.table.filler-1.0.js
@@ -0,0 +1,105 @@
+(function($) {
+
+    var settings = {
+        data: null,
+        dataUrl: '',
+        loader: false,
+        tableInstance: null,
+        headersSize: 0,
+        columnsSize: 0,
+        emptyDataMessage: 'No data found'
+    };
+
+    var methods = {
+
+        init : function(options) {
+            if (options) {
+                $.extend(settings, options);
+            }
+
+            settings.tableInstance = this;
+            $(settings.tableInstance).show();
+
+            if (settings.data != null) {
+                methods.analyzeData(settings.data.data);
+            } else {
+                methods.loadData(settings.dataUrl);
+            }
+        },
+        analyzeData: function (data) {
+            if (data && data.headers) {
+                methods.buildHeader(data.headers);
+            }
+            settings.headersSize = $('thead tr th',settings.tableInstance).size();
+
+            if (data && data.rows) {
+                methods.fillTable(data.rows);
+            } else {
+                methods.handleNoDataFound(null);
+            }
+        },
+        loadData: function(uri) {
+            $.ajax({
+                url: uri,
+                success: function (response, textStatus, xhr) {
+                    if (response && response.data) {
+                        methods.analyzeData(response.data)
+                    }
+                },
+                error: function (xhr, textStatus, errorThrown) {
+                    methods.handleLoadDataError(textStatus);
+                }
+            });
+        },
+        fillTable: function (rows) {
+            $('tbody', settings.tableInstance).remove();
+            $(settings.tableInstance).append('<tbody></tbody>');
+
+            if (rows.length > 0) {
+                for (var rowIndex in rows) {
+                    $('tbody', settings.tableInstance).append('<tr></tr>');
+                    var row = rows[rowIndex];
+                    for (var columnsIndex in row) {
+                        var columns = row[columnsIndex];
+                        for (var ind in columns) {
+                            $('tbody tr:last', settings.tableInstance).append('<td>' + columns[ind] + '</td>');
+                        }
+                    }
+                }
+            } else {
+                methods.handleNoDataFound();
+            }
+        },
+        buildHeader: function(headers) {
+            $('thead', settings.tableInstance).remove();
+            $(settings.tableInstance).prepend('<thead></thead>');
+            $("thead", settings.tableInstance).append('<tr></tr>');
+            for (var headerIndex in headers) {
+                $('thead tr:first', settings.tableInstance).append('<th>' + headers[headerIndex] + '</th>');
+            }
+        },
+        handleNoDataFound: function() {
+            $('tbody', settings.tableInstance).remove();
+            $(settings.tableInstance).append('<tbody></tbody>');
+            $('tbody', settings.tableInstance).append('<tr><td colspan="' + settings.headersSize + '">' + settings.emptyDataMessage + '</td></tr>');
+        },
+        handleLoadDataError: function(msg) {
+            alert(msg);
+            $("#tableFillerErrorMessage").empty();
+            $("#tableFillerErrorMessage").remove();
+            $(settings.tableInstance).hide();
+            $("body").append('<div id="tableFillerErrorMessage"><span>Error while loading data - ' + textStatus + '</span></div>');
+        }
+    };
+
+    $.fn.tableFiller = function( method ) {
+        if ( methods[method] ) {
+            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  method + ' does not exist on jQuery.tableFiller' );
+        }
+    };
+
+})(jQuery);
