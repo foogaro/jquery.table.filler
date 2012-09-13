921ff098a373c98937363fc74e2191afec488572
Luigi Fugaro <l.fugaro@gmail.com>
2012-09-13 15:15:09 +0200

    Pagination enabled

--- js/jquery.table.filler-2.0.js
+++ js/jquery.table.filler-2.0.js
@@ -16,7 +16,12 @@
         tableInstance: null,
         headersSize: 0,
         columnsSize: 0,
-        emptyDataMessage: 'No data found'
+        emptyDataMessage: 'No data found',
+        defaultPageSize: 25,
+        firstPageIndex: 0,
+        totalRows:0,
+        totalPages: 0,
+        currentPageIndex: 0
     };
 
     var methods = {
@@ -29,11 +34,15 @@
             settings.tableInstance = this;
             $(settings.tableInstance).show();
 
+            methods.process();
+        },
+        process: function() {
             if (settings.data != null) {
                 methods.analyzeData(settings.data.data);
             } else {
                 methods.loadData(settings.dataUrl);
             }
+            methods.buildPagerDashboard();
         },
         analyzeData: function (data) {
             if (data && data.headers) {
@@ -66,13 +75,17 @@
 
             if (rows.length > 0) {
                 for (var rowIndex in rows) {
-                    $('tbody', settings.tableInstance).append('<tr></tr>');
-                    var row = rows[rowIndex];
-                    for (var columnsIndex in row) {
-                        var columns = row[columnsIndex];
-                        for (var ind in columns) {
-                            $('tbody tr:last', settings.tableInstance).append('<td>' + columns[ind] + '</td>');
+                    if (rowIndex < settings.defaultPageSize) {
+                        $('tbody', settings.tableInstance).append('<tr></tr>');
+                        var row = rows[rowIndex];
+                        for (var columnsIndex in row) {
+                            var columns = row[columnsIndex];
+                            for (var ind in columns) {
+                                $('tbody tr:last', settings.tableInstance).append('<td>' + columns[ind] + '</td>');
+                            }
                         }
+                    } else {
+                        break;
                     }
                 }
             } else {
@@ -87,6 +100,36 @@
                 $('thead tr:first', settings.tableInstance).append('<th>' + headers[headerIndex] + '</th>');
             }
         },
+        buildPagerDashboard: function () {
+            $("#cntPagerTop").empty();
+            $("#cntPagerTop").html('');
+            var htmlFirstPrevious = '<ul class="ui-widget ui-helper-clearfix"><li title=".ui-icon-seek-first" class="ui-state-default ui-corner-all"><span class="ui-icon ui-icon-seek-first"></span></li><li title=".ui-icon-seek-prev" class="ui-state-default ui-corner-all"><span class="ui-icon ui-icon-seek-prev"></span></li></ul>';
+            var htmlNextLast = '<ul class="ui-widget ui-helper-clearfix"><li title=".ui-icon-seek-next" class="ui-state-default ui-corner-all"><span class="ui-icon ui-icon-seek-next"></span></li><li title=".ui-icon-seek-end" class="ui-state-default ui-corner-all"><span class="ui-icon ui-icon-seek-end"></span></li></ul>';
+            var htmlPager = '<span>' + htmlFirstPrevious + '</span><span>page ' + (settings.currentPageIndex+1) + ' of ' + settings.totalPages + '(' + settings.totalRows + ')' + '</span><span>' + htmlNextLast + '</span>';
+            $("#cntPagerTop").append(htmlPager);
+            //$("#cntPagerBottom").append(htmlPager);
+
+            if (settings.currentPageIndex > 0) {
+                $('span:first ul li:first', $("#cntPagerTop")).click(function (){
+                    methods.firstPage();
+                });
+            }
+            if (settings.currentPageIndex > 0) {
+                $('span:first ul li:last', $("#cntPagerTop")).click(function (){
+                    methods.previousPage();
+                });
+            }
+            if (settings.currentPageIndex >= 0 && settings.currentPageIndex < settings.totalPages-1) {
+                $("#cntPagerTop > span:last ul li:first").click(function (){
+                    methods.nextPage();
+                });
+            }
+            if (settings.currentPageIndex >=0 && settings.currentPageIndex < settings.totalPages-1) {
+                $("#cntPagerTop > span:last ul li:last").click(function (){
+                    methods.lastPage();
+                });
+            }
+        },
         handleNoDataFound: function() {
             $('tbody', settings.tableInstance).remove();
             $(settings.tableInstance).append('<tbody></tbody>');
@@ -98,6 +141,27 @@
             $("#tableFillerErrorMessage").remove();
             $(settings.tableInstance).hide();
             $("body").append('<div id="tableFillerErrorMessage"><span>Error while loading data - ' + textStatus + '</span></div>');
+        },
+        firstPage: function() {
+            settings.currentPageIndex = 0;
+            methods.moveToPage();
+        },
+        previousPage: function() {
+            settings.currentPageIndex--;
+            methods.moveToPage();
+        },
+        nextPage: function() {
+            settings.currentPageIndex++;
+            methods.moveToPage();
+        },
+        lastPage: function() {
+            settings.currentPageIndex = settings.totalPages-1;
+            methods.moveToPage();
+        },
+        moveToPage: function () {
+            //methods.loadData(settings.dataUrl + "/" + settings.currentPageIndex + "/" + settings.defaultPageSize);
+            methods.analyzeData(localData[settings.currentPageIndex].data);
+            methods.buildPagerDashboard();
         }
     };
 
